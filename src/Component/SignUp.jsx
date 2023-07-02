d90703e9a62233479fa5008f2fd1f6fdaf2354f6
Devrata puri <68563387+devratapuri@users.noreply.github.com>
2023-07-03 01:36:41 +0530

    Update SignUp.jsx

--- src/Component/SignUp.jsx
+++ src/Component/SignUp.jsx
@@ -1,7 +1,7 @@
-"use client"
+"use client";
 import axios from "axios";
 import { useState } from "react";
-import { AppBar,Toolbar,Typography,Button,TextField } from "@mui/material";
+import { AppBar, Toolbar, Typography, Button, TextField } from "@mui/material";
 
 const signUp = async (email, password, firstName, lastName) => {
   const response = await axios.post("https://reqres.in/api/users", {
@@ -29,44 +29,103 @@ const SignUp = () => {
   };
 
   return (
-    <div style={{ backgroundColor: "white",display:"flex"  }} >
-        {/* <AppBar position="static">
-        <Toolbar style={{ backgroundColor: "white" }}>
-        <Typography variant="h6" color="Black">
-          My App
-        </Typography>
-             </Toolbar>
-
-        </AppBar> */}
-        <Typography variant="h3" color ="black" align="center">
-        Sign In To your Account<br /> <br /> 
-    </Typography>
-    <Typography variant="h5" color ="black" align="center">
-    Build skills for today, tomorrow, and beyond.<br></br>
-    <center>Education to future-proof your career.</center><br></br>
-    </Typography>
-    <center>
-
-    <div className="inline-block">
-   <Button variant="outlined" style={{ display: "inline-block" ,margin: "10px"}} >Login from google</Button>
-    <Button variant="outlined" style={{ display: "inline-block" ,margin: "10px"}}>Login from Facebook</Button><br />
-    <hr style={{"width":"40%",display: "inline-block", fontWeight: "bold"}} />
-    Or
-    <hr style={{"width":"40%",display: "inline-block", fontWeight: "bold"}} />
-    
-    <form onSubmit={handleSignUp}>
-      <TextField id="outlined-basic" label="First Name" variant="outlined" style={{"width":"25%"}}/><br/><br />
-      <TextField id="outlined-basic" label="Last Name" variant="outlined" style={{"width":"25%"}}/><br/><br />
-      <TextField id="outlined-basic" label="EmailId" variant="outlined" style={{"width":"25%"}}/><br/><br />
-      <TextField id="outlined-basic" label="password" variant="outlined" style={{"width":"25%"}}/><br/><br />
-      <div style={{"fontSize":"10px","color":"Black"}}>
-      By clicking "Sign in," you agree to our Terms of Use and our Privacy Policy.<br/>
-        </div><br />
-        <Button   style={{"background-color": "blue" }}variant="contained" type="submit">Sign In</Button>
-      </form>
-      {user && <p>You have successfully signed up!</p>}
-    </div>
-    </center>
+    <div style={{ backgroundColor: "white" }}>
+      <Typography variant="h4" color="black" align="center">
+       <b>
+        Sign Up To your Account
+        </b> 
+        <br /><br></br>
+      </Typography>
+      <Typography variant="h5" color="black" align="center">
+        Build skills for today, tomorrow, and beyond.<br></br>
+        <center>Education to future-proof your career.</center>
+        <br></br>
+      </Typography>
+      <center>
+        <div className="inline-block">
+          <Button
+            variant="outlined"
+            style={{ display: "inline-block", margin: "10px" }}
+          >
+                <div style={{"margin-right":"10px",display : "inline-block"}}>
+   <img width="18" height="18" src="https://img.icons8.com/color/48/google-logo.png" alt="google-logo"/> </div>
+            Login from google
+          </Button>
+          <Button
+            variant="outlined"
+            style={{ display: "inline-block", margin: "10px" }}
+          >
+                  <div  style={{"margin-right":"10px",display : "inline-block"}}>
+    <img width="18" height="20" src="https://img.icons8.com/fluency/48/facebook.png" alt="facebook"/>
+      </div>
+            Login from Facebook
+          </Button>
+          <br />
+          <hr
+            style={{
+              width: "40%",
+              display: "inline-block",
+              fontWeight: "bold",
+            }}
+          />
+          Or
+          <hr
+            style={{
+              width: "40%",
+              display: "inline-block",
+              fontWeight: "bold",
+            }}
+          />
+          <form onSubmit={handleSignUp}>
+            <TextField
+              id="outlined-basic"
+              label="First Name"
+              variant="outlined"
+              style={{ width: "85%" }}
+            />
+            <br />
+            <br />
+            <TextField
+              id="outlined-basic"
+              label="Last Name"
+              variant="outlined"
+              style={{ width: "85%" }}
+            />
+            <br />
+            <br />
+            <TextField
+              id="outlined-basic"
+              label="EmailId"
+              variant="outlined"
+              style={{ width: "85%" }}
+            />
+            <br />
+            <br />
+            <TextField
+              id="outlined-basic"
+              label="password"
+              variant="outlined"
+              style={{ width: "85%" }}
+            />
+            <br />
+            <br />
+            <div style={{ fontSize: "10px", color: "Black" }}>
+              By clicking "Sign in," you agree to our Terms of Use and our
+              Privacy Policy.
+              <br />
+            </div>
+            <br />
+            <Button
+              style={{ "background-color": "blue" }}
+              variant="contained"
+              type="submit"
+            >
+              Sign In
+            </Button>
+          </form>
+          {user && <p>You have successfully signed up!</p>}
+        </div>
+      </center>
     </div>
   );
 };
